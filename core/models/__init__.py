ebd5fbce9536ce4d0c0909b58664b6fc94563388
M0V1ES <valera.lomov2016@yandex.ru>
2025-01-21 19:07:48 +0300

    update core\models to connect with db

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -4,9 +4,11 @@ __all__ = (
     "User",
     "DatabaseHelper",
     "db_helper",
+    "Author",
 )
 
 from .base import Base
 from .books import Book
 from .users import User
 from .db_helper import db_helper, DatabaseHelper
+from .authors import Author
